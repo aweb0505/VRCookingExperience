c89c865829b1ad95c7ca46763e5ea048d586a4d4
Aidan <aidanwebster987@gmail.com>
2021-03-27 23:17:46 -0500

    Adding all unity files

---
+++ VR Cooking Experience/Assets/Scripts/FoodItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : MonoBehaviour
+{
+
+    public MeshRenderer meshRenderer;
+
+    public float cookRate;
+
+    private bool isCooking;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCooking)
+        {
+            meshRenderer.material.color = Color.Lerp(meshRenderer.material.color, Color.black, cookRate * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("CookSurface"))
+        {
+            isCooking = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("CookSurface"))
+        {
+            isCooking = false;
+        }
+    }
+}
---
+++ VR Cooking Experience/Assets/ZenvaVR/Toolkit/Scripts/VrPointer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zenva.VR
+{
+    public class VrPointer : MonoBehaviour
+    {
+        [Tooltip("Maximum interaction distance")]
+        public float maxDistance;
+
+        [Tooltip("Show ray for debugging")]
+        public bool isDebug;
+
+        [Tooltip("Start position of the pointer")]
+        public Transform origin;
+
+        [Tooltip("Layers we want the pointer to detect")]
+        public LayerMask detectedLayers = ~0; 
+
+        // end position of the pointer
+        public Vector3 EndPosition { get; private set; }
+
+        // raycast hit
+        public RaycastHit Target { get; private set; }
+
+        // keep track of interactables
+        Interactable currInteractable;
+        Interactable prevInteractable;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Raycast();
+        }
+
+        void Raycast()
+        {
+            RaycastHit target;
+
+            // we found an object
+            if (Physics.Raycast(origin.position, transform.forward, out target, maxDistance, detectedLayers.value))
+            {
+                Target = target;
+
+                EndPosition = target.point;
+
+                currInteractable = target.collider.transform.GetComponent<Interactable>();
+
+                // call selection method
+                if (currInteractable)
+                    currInteractable.Over();
+            }
+
+            // we didn't find any object
+            else
+            {
+                Target = new RaycastHit();
+
+                EndPosition = transform.position + transform.forward * maxDistance;
+
+                // call the unselection method
+                if (currInteractable)
+                    currInteractable.Out();
+            }
+
+            // check that selection changed
+            if(currInteractable != prevInteractable)
+            {
+                prevInteractable?.Out();
+            }
+
+            prevInteractable = currInteractable;
+
+            if (true)
+                Debug.DrawRay(transform.position, EndPosition - transform.position, Color.blue);
+        }
+
+        public void PressButton()
+        {
+            currInteractable?.ButtonDown();
+        }
+    }
+}
